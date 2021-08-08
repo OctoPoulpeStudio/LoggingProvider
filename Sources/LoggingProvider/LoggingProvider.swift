e774c5fddb6843a11a4621bb675b86b9bea4d54d
Guillaume Le Souchu <lesouchu@gmail.com>
2021-08-08 13:50:25 +0200

    [LOG-1] transform the struct into class

--- Sources/LoggingProvider/LoggingProvider.swift
+++ Sources/LoggingProvider/LoggingProvider.swift
@@ -1,5 +1,5 @@
 import os.log
-struct LoggingProvider {
+public class LoggingProvider {
 	public let subsystem:String
 	
 	private var loggers:[String:LogWrapper] = [:]
@@ -9,13 +9,14 @@ struct LoggingProvider {
 		self.subsystem = subsystem
 	}
 	
-	public mutating func getLogger(forCategory category:String)->LogWrapper
+	public func getLogger(forCategory category:String)->LogWrapper
 	{
 		return loggers[category] ?? createNewLogger(forCategory: category)
 	}
 	
-	private mutating func createNewLogger(forCategory category:String)->LogWrapper
+	private func createNewLogger(forCategory category:String)->LogWrapper
 	{
+		
 		if #available(iOS 14, *) {
 			let logger = LoggerWrapper(subsystem: subsystem, category: category)
 			loggers[category] = logger
@@ -32,31 +33,31 @@ struct LoggingProvider {
 		}
 	}
 	
-	public mutating func log(level: OSLogType, _ message:String, forCategory category:String){
+	public func log(level: OSLogType, _ message:String, forCategory category:String){
 		getLogger(forCategory: category).log(level: level, message)
 	}
-	public mutating func debug(_ message:String, forCategory category:String){
+	public func debug(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).debug(message)
 	}
-	public mutating func trace(_ message:String, forCategory category:String){
+	public func trace(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).trace(message)
 	}
-	public mutating func info(_ message:String, forCategory category:String){
+	public func info(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).info(message)
 	}
-	public mutating func notice(_ message:String, forCategory category:String){
+	public func notice(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).notice(message)
 	}
-	public mutating func error(_ message:String, forCategory category:String){
+	public func error(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).error(message)
 	}
-	public mutating func warning(_ message:String, forCategory category:String){
+	public func warning(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).warning(message)
 	}
-	public mutating func critical(_ message:String, forCategory category:String){
+	public func critical(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).critical(message)
 	}
-	public mutating func fault(_ message:String, forCategory category:String){
+	public func fault(_ message:String, forCategory category:String){
 		getLogger(forCategory: category).fault(message)
 	}
 }
